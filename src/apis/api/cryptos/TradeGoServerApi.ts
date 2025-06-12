a10ce08ecacfd86051a0a586d1bf85c5081f52f1
JunYub Kim <kjyub08@naver.com>
2025-06-12 17:59:17 +0900

    테스트 코드 제거

--- src/apis/api/cryptos/TradeGoServerApi.ts
+++ src/apis/api/cryptos/TradeGoServerApi.ts
@@ -4,20 +4,19 @@ import type { IUpbitMarketTicker } from '@/types/cryptos/CryptoInterfaces';
 namespace TradeGoServerApi {
   // region Market
   export async function getMarketCurrent(marketCode: string): Promise<IUpbitMarketTicker> {
-    const result: IUpbitMarketTicker = {} as IUpbitMarketTicker;
+    let result: IUpbitMarketTicker = {} as IUpbitMarketTicker;
 
     try {
-      // const response = await tradeInstance.post('markets', {
-      //   json: {
-      //     codes: [marketCode],
-      //   },
-      // });
-      // console.log('getMarketCurrent response', response.url);
-      // const data = (await response.json()) as any;
-      // if (Array.isArray(data) && data.length > 0) {
-      //   result = data[0];
-      // }
-      return {} as IUpbitMarketTicker;
+      const response = await tradeInstance.post('markets', {
+        json: {
+          codes: [marketCode],
+        },
+      });
+      console.log('getMarketCurrent response', response.url);
+      const data = (await response.json()) as any;
+      if (Array.isArray(data) && data.length > 0) {
+        result = data[0];
+      }
     } catch (error) {
       console.log('getMarketCurrent error');
       console.log(error);
