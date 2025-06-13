6ec957f236a54cde8f6c21bb7a4464739b29f720
JunYub Kim <kjyub08@naver.com>
2025-06-14 03:56:58 +0900

    trade api 요청 hook 테스트

--- src/apis/utils/tradeInstances.ts
+++ src/apis/utils/tradeInstances.ts
@@ -3,7 +3,18 @@ import ky, { type Options } from 'ky';
 const URL = process.env.NEXT_PUBLIC_TRADE_SERVER;
 
 const createKyInstance = (options?: Options): typeof ky => {
-  return ky.create({ prefixUrl: URL, ...options });
+  return ky.create({
+    prefixUrl: URL,
+    ...options,
+    timeout: 5000,
+    hooks: {
+      beforeRequest: [
+        (request) => {
+          request.headers.set('Connection', 'close');
+        },
+      ],
+    },
+  });
 };
 
 export const tradeInstance = createKyInstance();
