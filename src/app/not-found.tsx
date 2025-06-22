f65632f056f8d298d4ecc211cbb4638f6680e269
JunYub Kim <kjyub08@naver.com>
2025-06-23 04:19:30 +0900

    notFound 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link';
+import './notFoundStyle.css';
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>404 - Not Found</h1>
-      <p>The page you are looking for does not exissadasdt.</p>
+    <div className="flex flex-col flex-center w-full h-dvh">
+      <h1 className="text-9xl font-bold text-slate-50/50">
+        <span className="notfound-animation">4</span>
+        <span className="notfound-animation">0</span>
+        <span className="notfound-animation">4</span>
+      </h1>
+      <p className="text-slate-300/70">페이지를 찾을 수 없습니다</p>
+      <Link href="/" className="px-6 py-4 mt-4 rounded-2xl bg-slate-100/10 hover:!bg-slate-100/20 !text-slate-300/80">
+        메인으로 돌아가기
+      </Link>
     </div>
   );
 }
